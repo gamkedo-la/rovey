2893ba289527561c43d96f81c14e6b7483f5bf00
Tyler Funk <tcfunk25@gmail.com>
2020-08-26 13:17:48 -0500

    Initial commit, copy from prototype

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 6f;
+    public float turnSpeed = 6f;
+    public float gravity = 2f;
+    public float jumpStrength = 1f;
+
+    private CharacterController controller;
+    public Transform cam;
+
+    private Vector3 velocity;
+    private float smoothTurnVelocity;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main.transform;
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        var horizontal = Input.GetAxisRaw("Horizontal");
+        transform.Rotate(transform.up, horizontal * turnSpeed * Time.deltaTime);
+
+        var forward = Input.GetAxisRaw("Vertical");
+        var inputDirection = new Vector3(0f, 0f, forward).normalized;
+        var transformDirection = transform.TransformDirection(inputDirection);
+
+        var flatMovement = speed * Time.deltaTime * transformDirection;
+        velocity = new Vector3(flatMovement.x, velocity.y, flatMovement.z);
+
+        if (PlayerJumped)
+            velocity.y = jumpStrength;
+        else if (controller.isGrounded)
+            velocity.y = 0f;
+        else
+            velocity.y -= gravity * Time.deltaTime;
+
+        controller.Move(velocity);
+    }
+
+    private bool PlayerJumped => controller.isGrounded && Input.GetAxisRaw("Jump") >= 0.1f;
+}
---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    // Player object to follow
+    public Transform player;
+    private Vector3 pan = Vector3.zero;
+
+    // Control how quickly the camera catches up with player
+    // movement and rotation
+    public float translationDamping = 5f;
+    public float rotationDamping = 5f;
+    
+    // Camera in hierarchy
+    private Transform camera;
+    
+    void Start()
+    {
+        camera = gameObject.GetComponentInChildren<Camera>().transform;
+    }
+
+    private void LateUpdate()
+    {
+        // Catch up with the player
+        transform.position = Vector3.Lerp(transform.position, player.position, Time.deltaTime * translationDamping);
+        
+        // Look in the same direction as the player
+        pan.y = player.rotation.eulerAngles.y;
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(pan), Time.deltaTime * rotationDamping);
+    }
+}
