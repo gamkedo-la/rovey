ea0d13f135774d2696f0922acfd0cd495287483b
Christer Kaitila <breakdance@mcfunkypants.com>
2020-09-04 16:19:32 -0700

    figured out a way to do URP projectors: an negative spotlight!

---
+++ Assets/Scripts/funkyBlackLight.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// this simple class inverts a light colour
+// from white (or whatever) to black
+// which is used to hack spotlights
+// into acting like a "projector"
+// that works in URP rendering mode
+public class funkyBlackLight : MonoBehaviour
+{
+    void Start() {
+       Debug.Log("Turning a regular light into a BLACK LIGHT!");
+       Light light = GetComponent<Light>();
+       light.color = new Color(-1,-1,-1,1);
+       light.intensity = 50;
+    }
+}
