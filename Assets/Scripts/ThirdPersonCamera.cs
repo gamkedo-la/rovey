43e2df0bd56235f3ed2ce78dc7ed2751f70aaca2
Tyler Funk <tcfunk25@gmail.com>
2020-09-09 06:54:00 -0500

    Fix mouse-based camera controls

--- Assets/Scripts/ThirdPersonCamera.cs
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -9,11 +9,16 @@ public class ThirdPersonCamera : MonoBehaviour
 {
     private CinemachineFreeLook freeLook;
 
+    // Holding the left mouse button down will temporarily
+    // change these to "Mouse X" and "Mouse Y".
+    private string defaultHorizontalAxis;
+    private string defaultVerticalAxis;
+
     void Start()
     {
         freeLook = (CinemachineFreeLook) GetComponentInChildren<CinemachineBrain>().ActiveVirtualCamera;
-        // transform.parent = transform.root;
-        Debug.Log(transform.root);
+        defaultHorizontalAxis = freeLook.m_XAxis.m_InputAxisName;
+        defaultVerticalAxis = freeLook.m_YAxis.m_InputAxisName;
     }
 
     private void LateUpdate()
@@ -21,15 +26,15 @@ public class ThirdPersonCamera : MonoBehaviour
         // Adjust camera with mouse while holding left click.
         if (Input.GetMouseButton(0))
         {
-            freeLook.m_XAxis.m_InputAxisValue = Input.GetAxis("Mouse X");
-            freeLook.m_YAxis.m_InputAxisValue = Input.GetAxis("Mouse Y");
+            freeLook.m_XAxis.m_InputAxisName = "Mouse X";
+            freeLook.m_YAxis.m_InputAxisName = "Mouse Y";
         }
 
         // Stop camera from getting stuck with last mousex/y axis value.
         if (Input.GetMouseButtonUp(0))
         {
-            freeLook.m_XAxis.m_InputAxisValue = 0;
-            freeLook.m_YAxis.m_InputAxisValue = 0;
+            freeLook.m_XAxis.m_InputAxisName = defaultHorizontalAxis;
+            freeLook.m_YAxis.m_InputAxisName = defaultVerticalAxis;
         }
     }
 }
