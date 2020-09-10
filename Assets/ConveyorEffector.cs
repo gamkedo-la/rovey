f4c148ab9ff509196c360cf47a2de60c2738f9ba
Tyler Funk <tcfunk25@gmail.com>
2020-09-10 17:05:57 -0500

    Added conveyor platform logic

---
+++ Assets/ConveyorEffector.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class ConveyorEffector : MonoBehaviour
+{
+    // Make sure this has a downward direction too, so that the CharacterController stays grounded.
+    public Vector3 effectDirection = Vector3.forward;
+
+    private void OnTriggerStay(Collider other)
+    {
+        // For player, use character controller to move; otherwise, just use transform.position.
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<CharacterController>().Move(effectDirection * Time.fixedDeltaTime);
+        }
+        else
+        {
+            // For everything else, use Rigidbody if it has one, otherwise just move the position of the object.
+            // TODO: Probably want to change how broadly this works at some point or our conveyor belt is going to be scooting around pieces of environment.
+            var otherRigidbody = other.GetComponent<Rigidbody>();
+            if (otherRigidbody)
+            {
+                var currentPosition = other.transform.position;
+                otherRigidbody.MovePosition(currentPosition + (effectDirection * Time.fixedDeltaTime));
+            }
+            else
+            {
+                other.transform.Translate(effectDirection * Time.fixedDeltaTime);
+            }
+        }
+    }
+}
