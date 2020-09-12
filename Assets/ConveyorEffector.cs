893d4905b94343d304be41e6b8d07d5211a22172
Tyler Funk <tcfunk25@gmail.com>
2020-09-11 22:52:58 -0500

    Add conveyor belt model; animation belt texture

--- Assets/ConveyorEffector.cs
+++ Assets/ConveyorEffector.cs
@@ -1,9 +1,37 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class ConveyorEffector : MonoBehaviour
 {
     // Make sure this has a downward direction too, so that the CharacterController stays grounded.
     public Vector3 effectDirection = Vector3.forward;
+
+    public MeshRenderer conveyorBeltMesh;
+    public float materialOffsetFraction = 0.001f;
+    private Material conveyorMaterial;
+    private Vector2 currentMaterialOffset = Vector2.zero;
+
+    private void Start()
+    {
+        // Find the right material in the list and store a reference.
+        List<Material> materials = new List<Material>();
+        conveyorBeltMesh.GetMaterials(materials);
+        foreach (var material in materials)
+        {
+            if (material.name.Contains("conveyor_belt"))
+            {
+                conveyorMaterial = material;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        // Change material offset to animate the conveyor belt
+        currentMaterialOffset.y += effectDirection.z * materialOffsetFraction * Time.deltaTime;
+        conveyorMaterial.mainTextureOffset = currentMaterialOffset;
+    }
 
     private void OnTriggerStay(Collider other)
     {
