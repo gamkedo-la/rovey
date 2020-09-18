d7e2af826b08fde35bc695074f4823dbfc059e9f
Tyler Funk <tcfunk25@gmail.com>
2020-09-17 20:57:53 -0500

    Moving platform waypoint system

--- Assets/Scripts/WaypointMovement.cs
+++ Assets/Scripts/WaypointMovement.cs
@@ -10,17 +10,17 @@ public class WaypointMovement : MonoBehaviour
     public AnimationCurve transitionCurve;
     [Tooltip("Transition time in seconds between waypoints")]
     public float transitionTime;
+    [Tooltip("Time spent at each waypoint before transitioning to the next")]
+    public float waypointTransitionDelay = 1.0f;
 
-    private int targetWaypointIndex;
-    private int currentWaypointIndex;
-    private float currentTransitionTime;
+    private int currentWaypointIndex = 0;
+    private int targetWaypointIndex = 1;
+    private float currentTransitionTime = 0f;
+    private bool waitingToTransition = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        currentWaypointIndex = 0;
-        currentTransitionTime = 0;
-        targetWaypointIndex = 1;
         transform.position = waypoints[0].position;
     }
 
@@ -35,10 +35,21 @@ public class WaypointMovement : MonoBehaviour
             transitionCurve.Evaluate(currentTransitionTime / transitionTime));
         var sqrDistanceToTarget = (targetWaypointPosition - transform.position).sqrMagnitude;
 
-        if (sqrDistanceToTarget < 0.1f)
+        // Transition to next waypoint once maximum transition time is reached.
+        if (currentTransitionTime > transitionTime && !waitingToTransition)
         {
-            currentWaypointIndex = ++currentWaypointIndex % waypoints.Count;
-            currentTransitionTime = 0f;
+            waitingToTransition = true;
+            StartCoroutine(DelayedWaypointTransition());
         }
+    }
+
+    private IEnumerator DelayedWaypointTransition()
+    {
+        yield return new WaitForSeconds(waypointTransitionDelay);
+
+        currentWaypointIndex = ++currentWaypointIndex % waypoints.Count;
+        targetWaypointIndex = ++targetWaypointIndex % waypoints.Count;
+        currentTransitionTime = 0f;
+        waitingToTransition = false;
     }
 }
