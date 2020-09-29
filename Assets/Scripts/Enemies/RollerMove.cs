d079baebbdaf3e570c6ee04d01c23ee21e385baa
Xist3nce <Xmedia101@yahoo.com>
2020-09-29 15:48:22 -0400

    RollerEnemyInitial - NavMesh prep

---
+++ Assets/Scripts/Enemies/RollerMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class RollerMove : MonoBehaviour
+{
+
+    [SerializeField]
+     private Transform _destination;
+    [SerializeField]
+     private NavMeshAgent _navMeshAgent;
+
+    void Start()
+    {
+        if (_navMeshAgent == null)
+        {
+            return;
+        }
+
+        else
+        {
+            SetDestination();
+        }
+    }
+
+    private void SetDestination()
+    {
+        if (_destination != null)
+        {
+            Vector3 targetVector = _destination.transform.position;
+            _navMeshAgent.SetDestination(targetVector);
+        }
+    }
+    void Update()
+    {
+        
+    }
+}
