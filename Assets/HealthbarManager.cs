7ffc43e489bf0a71f5bedf1f578ae5929d6083a1
Tyler Funk <tcfunk25@gmail.com>
2020-10-02 15:02:04 -0500

    UI prefab and healthbar manager

--- Assets/HealthbarManager.cs
+++ Assets/HealthbarManager.cs
@@ -1,36 +1,77 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthbarManager : MonoBehaviour
 {
     public GameObject healthIndicatorWidget;
     public int playerMaxHealth = 5;
-    private int playerCurrentHealth;
+    public Color lowColor;
+    public Color highColor;
+    public Color extraColor;
 
     private List<GameObject> widgets;
+    private Color[] colors;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerCurrentHealth = playerMaxHealth;
+        colors = new Color[5];
+        SetupColors();
+
+        RemoveHealth(transform.childCount);
         AddHealth(playerMaxHealth);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftBracket))
+        {
+            RemoveHealth();
+        }
+        else if (Input.GetKeyDown(KeyCode.RightBracket))
+        {
+            AddHealth();
+        }
+    }
+
+    private void SetupColors()
+    {
+        Color.RGBToHSV(lowColor, out float lHue, out float lSaturation, out float lValue);
+        Color.RGBToHSV(highColor, out float hHue, out _, out _);
+        var stepSize = (hHue - lHue) / (playerMaxHealth - 1);
+
+        for (var i = 0; i < playerMaxHealth; i++)
+        {
+            var hue = lHue + (stepSize * i);
+            colors[i] = Color.HSVToRGB(hue, lSaturation, lValue);
+        }
     }
 
     public void AddHealth(int amount = 1)
     {
-        for (int i = 0; i < amount; i++)
+        for (var i = 0; i < amount; i++)
         {
-            Instantiate(healthIndicatorWidget, Vector3.zero, Quaternion.identity, transform);
+            var ind = Instantiate(healthIndicatorWidget, Vector3.zero, Quaternion.identity, transform);
+            var colorIndex = transform.childCount - 1;
+            var color = (colorIndex > colors.Length - 1) ? extraColor : colors[colorIndex];
+            ind.GetComponent<CanvasRenderer>()?.SetColor(color);
         }
     }
 
     public void RemoveHealth(int amount = 1)
     {
-        var targetHealth = playerCurrentHealth - amount;
-        while (transform.childCount > targetHealth)
+        if (transform.childCount == 0) return;
+
+        var childCount = transform.childCount;
+        var amountToRemove = (childCount < amount) ? childCount : amount;
+
+        for (var i = 0; i < amountToRemove; i++)
         {
-            Destroy(transform.GetChild(0));
+            var child = transform.GetChild(transform.childCount - 1);
+            Destroy(child.gameObject);
         }
     }
 }
