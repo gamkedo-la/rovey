5de194498ae0feda8b9a1c58e71de27f2aa8dd07
brnielsen <47045697+brnielsen@users.noreply.github.com>
2020-10-12 18:01:06 -0400

    Rough Title Screen Layout

---
+++ Assets/Scripts/UI/AnimatorFunctions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//From Thomas Brush tutorial https://www.youtube.com/watch?v=vqZjZ6yv1lA
+
+public class AnimatorFunctions : MonoBehaviour
+{
+    [SerializeField] MenuButtonController menuButtonController;
+    public bool disableOnce;
+
+    void PlaySound(AudioClip audioClip)
+    {
+        if (!disableOnce)
+        {
+            menuButtonController.audioSource.PlayOneShot(audioClip);
+        }
+        else
+        {
+            disableOnce = false;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/MenuButton.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButton : MonoBehaviour
+{
+
+    //From Thomas Brush tutorial https://www.youtube.com/watch?v=vqZjZ6yv1lA
+
+    [SerializeField] MenuButtonController menuButtonController;
+    [SerializeField] Animator animator;
+    [SerializeField] AnimatorFunctions animatorFunctions;
+    [SerializeField] int thisIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (menuButtonController.index == thisIndex)
+        {
+            animator.SetBool("Selected", true);
+            if(Input.GetAxis("Submit") == 1)
+            {
+                animator.SetBool("Pressed", true);
+            }else if (animator.GetBool("Pressed"))
+            {
+                animator.SetBool("Pressed", false);
+                animatorFunctions.disableOnce = true;
+            }
+        }
+        else
+        {
+            animator.SetBool("Selected", false);
+        }
+    }
+}
