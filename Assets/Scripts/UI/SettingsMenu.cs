a3ed0afed4040dff77a5455aafb63f3ec208177f
Tyler Funk <tcfunk25@gmail.com>
2020-11-06 20:07:41 -0600

    WIP persistent application options

--- Assets/Scripts/UI/SettingsMenu.cs
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -51,16 +51,21 @@ public class SettingsMenu : MonoBehaviour
         var volumeAdjust = volume == 0 ? -80f : 10 * Mathf.Log10(volume);
         var adjustedVolume = Mathf.Clamp(baseVolume + volumeAdjust, -80f, 0f); // ensure bad math isn't exploding eardrums.
         audioMixer.SetFloat("volume", adjustedVolume);
+        PlayerPrefs.SetFloat("volume", volume);
     }
 
     public void SetResolution(int resolutionIndex)
     {
         Resolution resolution = resolutions[resolutionIndex];
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        PlayerPrefs.SetFloat("resolution.width", resolution.width);
+        PlayerPrefs.SetFloat("resolution.height", resolution.height);
+        PlayerPrefs.SetFloat("resolution.refreshRate", resolution.refreshRate);
     }
 
     public void SetFullscreen(bool isFullscreen)
     {
         Screen.fullScreen = isFullscreen;
+        PlayerPrefs.SetInt("fullscreen", isFullscreen ? 1 : 0);
     }
 }
