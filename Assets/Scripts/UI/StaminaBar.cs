fe62b687bf99f60e41d2d3560b1e3da257869db9
Tyler Funk <tcfunk25@gmail.com>
2020-11-12 20:01:34 -0600

    WIP stamina bar for hover timer

---
+++ Assets/Scripts/UI/StaminaBar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StaminaBar : MonoBehaviour
+{
+    public RectTransform staminaBar;
+
+    [Range(0f, 1.0f)] public float scaleSlider = 1.0f;
+
+    private void Update()
+    {
+        staminaBar.localScale = new Vector2(scaleSlider, 1f);
+    }
+}
