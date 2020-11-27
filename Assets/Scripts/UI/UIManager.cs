5b5ba14f3b200d22be1ff3965f926d00d3a5f5b1
Tyler Funk <tcfunk25@gmail.com>
2020-11-27 07:07:43 -0600

    added pause screen

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            TogglePauseMenu();
+        }
+    }
+
+    public void TogglePauseMenu()
+    {
+        if (pauseMenu)
+        {
+            pauseMenu.SetActive(!pauseMenu.activeInHierarchy);
+        }
+        else
+        {
+            Debug.LogError("No pause menu assigned in HUD.UIManager");
+        }
+    }
+}
