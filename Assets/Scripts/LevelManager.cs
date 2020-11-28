95b7b32de7cc7c95016a4185c1976a9450676d6e
Tyler Funk <tcfunk25@gmail.com>
2020-11-27 23:09:31 -0600

    super basic level complete & transition script

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public string nextLevel;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Check if all 3 items have been collected, transition to the next level.
+            var playerController = other.GetComponent<PlayerController>();
+            if (playerController.KeyItems >= 3)
+            {
+                SceneManager.LoadScene(nextLevel);
+            }
+        }
+    }
+}
