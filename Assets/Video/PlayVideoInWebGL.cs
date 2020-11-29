e604cc311693b8b8df7a0730998f930804d4f571
brnielsen <47045697+brnielsen@users.noreply.github.com>
2020-11-29 11:07:11 -0500

    Added script in case video won't play in webgl

---
+++ Assets/Video/PlayVideoInWebGL.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+public class PlayVideoInWebGL : MonoBehaviour
+{
+
+    [SerializeField]
+    private VideoPlayer videoPlayer;
+
+    [SerializeField]
+    private string videoFileName;
+
+    private string url;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (string.IsNullOrEmpty(url))
+        {
+            url = System.IO.Path.Combine(Application.streamingAssetsPath, videoFileName);
+        }
+        videoPlayer.url = url;
+    }
+
+
+}
